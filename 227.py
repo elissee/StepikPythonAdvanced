34333b628bd30578740f685f49cba74b2bc8b4c2
eliseev@lanit.ru <eliseev@lanit.ru>
2021-11-02 23:01:53 +0500

    227

--- 227.py
+++ 227.py
@@ -1,6 +1,22 @@
 tim = input()
 rus = input()
+tim_win = 'Тимур'
+rus_win = 'Руслан'
+draw = 'ничья'
 stone = 'камень'
 scissors = 'ножницы'
 paper = 'бумага'
-if tim == stone and rus == scissors
+if tim == rus:
+    print(draw)
+elif tim == stone and rus == scissors:
+    print(tim_win)
+elif tim == stone and rus == paper:
+    print(rus_win)
+elif tim == scissors and rus == stone:
+    print(rus_win)
+elif tim == scissors and rus == paper:
+    print(tim_win)
+elif tim == paper and rus == scissors:
+    print(rus_win)
+elif tim == paper and rus == stone:
+    print(tim_win)
