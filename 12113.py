85b192580f2e539604188deabb90fe4871b40fe0
Igor Eliseev <eliseev@lanit.ru>
2022-10-03 15:36:19 +0300

    12113

--- 12113.py
+++ 12113.py
@@ -3,5 +3,10 @@ import random
 length = int(input())    # длина пароля
 
 for i in range(length):
-    password = random.randint(65, 90)
-    print(chr(password), end='')
+    password1 = random.randint(65, 90)
+    password2 = random.randint(97, 122)
+    choice = random.randint(0, 1)
+    if choice == 0:
+        print(chr(password1), end='')
+    elif choice == 1:
+        print(chr(password2), end='')
