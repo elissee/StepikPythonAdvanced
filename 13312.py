71799a5207d495dcc1cf0575012ede8e8374384a
Igor Eliseev <eliseev@lanit.ru>
2023-05-03 17:00:49 +0300

    13312-13313

---
+++ 13312.py
@@ -0,0 +1,9 @@
+z1, z2 = complex(input()), complex(input())
+
+amount = z1 + z2
+dif = z1 - z2
+mul = z1 * z2
+
+print(z1, "+", z2, "=", amount)
+print(z1, "-", z2, "=", dif)
+print(z1, "*", z2, "=", mul)
